28fb08cc4d6df46a0fd3700bca09b4fc35ed5251
jvieser <julian.vieser@gmail.com>
2019-12-19 10:49:56 +0000

    Apply fixes from StyleCI

--- app/Console/Commands/epgdataImport.php
+++ app/Console/Commands/epgdataImport.php
@@ -37,26 +37,25 @@ class epgdataImport extends Command
     /**
      * Execute the console command.
      *
-     * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
+     *
+     * @return mixed
      */
     public function handle()
     {
-
         $epgUrls = [
             'epgPackageUrl0' => env('EPG_PACKAGE_URL_0'),
             'epgPackageUrl1' => env('EPG_PACKAGE_URL_1'),
             'epgPackageUrl2' => env('EPG_PACKAGE_URL_2'),
         ];
 
-        if(date('j') === 1) {// First day of the month should be enough
+        if (date('j') === 1) {// First day of the month should be enough
 
             $epgUrls['epgIncludeUrl'] = env('EPG_INCLUDES_URL');
 
             DB::table('epgdata_channel')->truncate();
             DB::table('epgdata_categoriy')->truncate();
             DB::table('epgdata_genre')->truncate();
-
         }
 
         DB::table('epgdata_movie')->truncate();
@@ -64,27 +63,23 @@ class epgdataImport extends Command
         /*
          * Download and extract files
          */
-        foreach ($epgUrls AS $name => $epgUrl) {
+        foreach ($epgUrls as $name => $epgUrl) {
+            $filename = date('Ymd', strtotime('+ 3 days')).'_'.$name.'.zip';
 
-            $filename = date('Ymd', strtotime('+ 3 days')) . '_' . $name . '.zip';
+            $localFile = storage_path('/tmp/'.$filename);
 
-            $localFile = storage_path('/tmp/' . $filename);
-
-            if (file_exists(storage_path('/tmp/' . $filename)) === false) {
-
+            if (file_exists(storage_path('/tmp/'.$filename)) === false) {
                 $client = new Client();
                 $client->request('GET', $epgUrl, [
-                    'sink' => $localFile
+                    'sink' => $localFile,
                 ]);
-
             }
 
-            $zip = new ZipArchive;
-            if($zip->open($localFile) === true) {
+            $zip = new ZipArchive();
+            if ($zip->open($localFile) === true) {
                 $zip->extractTo(storage_path('/tmp/'));
                 $zip->close();
             }
-
         }
 
         /*
@@ -92,113 +87,101 @@ class epgdataImport extends Command
          */
         $files = File::files(storage_path('/tmp'));
 
-        foreach($files AS $file) {
-
+        foreach ($files as $file) {
             if (substr($file->getPathname(), -4) === '.xml') {
-
                 $xml = simplexml_load_file($file->getPathname());
-                foreach ($xml AS $dom) {
-
-                    if(isset($dom->d0)) {// Movie file
+                foreach ($xml as $dom) {
+                    if (isset($dom->d0)) {// Movie file
 
                         $insert_data = [
-                            'id' => null,
-                            'broadcast_id' => (int) $dom->d0,
-                            'tvshow_id' => (int) $dom->d1,
-                            'tvchannel_id' => (int) $dom->d2,
-                            'tvregionid' => (int) $dom->d3,
-                            'starttime' => (string) $dom->d4,
-                            'endtime' => (string)$dom->d5,
-                            'broadcast_day' => (int) $dom->d6,
-                            'tvshow_length' => (int) $dom->d7,
-                            'vps' => (int) $dom->d8,
-                            'primetime' => (int) $dom->d9,
-                            'category_id' => (int)  $dom->d10,
-                            'technics_bw' => (int) $dom->d11,
+                            'id'                  => null,
+                            'broadcast_id'        => (int) $dom->d0,
+                            'tvshow_id'           => (int) $dom->d1,
+                            'tvchannel_id'        => (int) $dom->d2,
+                            'tvregionid'          => (int) $dom->d3,
+                            'starttime'           => (string) $dom->d4,
+                            'endtime'             => (string) $dom->d5,
+                            'broadcast_day'       => (int) $dom->d6,
+                            'tvshow_length'       => (int) $dom->d7,
+                            'vps'                 => (int) $dom->d8,
+                            'primetime'           => (int) $dom->d9,
+                            'category_id'         => (int) $dom->d10,
+                            'technics_bw'         => (int) $dom->d11,
                             'technics_co_channel' => (int) $dom->d12,
-                            'technics_vt150' => (int) $dom->d13,
-                            'technics_coded' => (int) $dom->d14,
-                            'technics_blind' => (int) $dom->d15,
-                            'age_marker' => (int) $dom->d16,
-                            'live_id' => (int) $dom->d17,
-                            'tipflag' => (int) $dom->d18,
-                            'title' => (string) $dom->d19,
-                            'subtitle' => (string) $dom->d20,
-                            'comment_long' => (string) $dom->d21,
-                            'comment_middle' => (string) $dom->d22,
-                            'comment_short' => (string) $dom->d23,
-                            'themes' => (int) $dom->d24,
-                            'genreid' => (int) $dom->d25,
-                            'sequence' => (int) $dom->d26,
-                            'technics_stereo' => (int) $dom->d27,
-                            'technics_dolby' => (int) $dom->d28,
-                            'technics_wide' => (int) $dom->d29,
-                            'tvd_total_value' => (int) $dom->d30,
-                            'attribute' => (int) $dom->d31,
-                            'country' => (string) $dom->d32,
-                            'year' => (int) $dom->d33,
-                            'moderator' => (string) $dom->d34,
-                            'studio_guest' => (string) $dom->d35,
-                            'regisseur' => (string) $dom->d36,
-                            'actor' => (string) $dom->d37,
-                            'image_small' => (string) $dom->d38,
-                            'image_middle' => (string) $dom->d39,
-                            'image_big' => (string) $dom->d40
+                            'technics_vt150'      => (int) $dom->d13,
+                            'technics_coded'      => (int) $dom->d14,
+                            'technics_blind'      => (int) $dom->d15,
+                            'age_marker'          => (int) $dom->d16,
+                            'live_id'             => (int) $dom->d17,
+                            'tipflag'             => (int) $dom->d18,
+                            'title'               => (string) $dom->d19,
+                            'subtitle'            => (string) $dom->d20,
+                            'comment_long'        => (string) $dom->d21,
+                            'comment_middle'      => (string) $dom->d22,
+                            'comment_short'       => (string) $dom->d23,
+                            'themes'              => (int) $dom->d24,
+                            'genreid'             => (int) $dom->d25,
+                            'sequence'            => (int) $dom->d26,
+                            'technics_stereo'     => (int) $dom->d27,
+                            'technics_dolby'      => (int) $dom->d28,
+                            'technics_wide'       => (int) $dom->d29,
+                            'tvd_total_value'     => (int) $dom->d30,
+                            'attribute'           => (int) $dom->d31,
+                            'country'             => (string) $dom->d32,
+                            'year'                => (int) $dom->d33,
+                            'moderator'           => (string) $dom->d34,
+                            'studio_guest'        => (string) $dom->d35,
+                            'regisseur'           => (string) $dom->d36,
+                            'actor'               => (string) $dom->d37,
+                            'image_small'         => (string) $dom->d38,
+                            'image_middle'        => (string) $dom->d39,
+                            'image_big'           => (string) $dom->d40,
 
                         ];
 
-                        if(empty($insert_data['moderator']) && empty($insert_data['studio_guest'])) {
+                        if (empty($insert_data['moderator']) && empty($insert_data['studio_guest'])) {
                             DB::table('epgdata_movie')->insert($insert_data);
                         }
-
                     }
 
-                    if(isset($dom->ca0)) {//Category file
+                    if (isset($dom->ca0)) {//Category file
 
                         $insert_data = [
-                            'id' => null,
+                            'id'        => null,
                             'category1' => (string) $dom->ca0,
                             'category2' => (string) $dom->ca1,
                         ];
 
                         DB::table('epgdata_categoriy')->insert($insert_data);
-
                     }
 
-                    if(isset($dom->ch0)) {//Channel file
+                    if (isset($dom->ch0)) {//Channel file
 
                         $insert_data = [
-                            'id' => null,
+                            'id'         => null,
                             'channel_id' => (int) $dom->ch4,
-                            'name' => (string) $dom->ch0,
-                            'language' => (string) $dom->ch3,
-                            'country' => (string) $dom->ch2,
+                            'name'       => (string) $dom->ch0,
+                            'language'   => (string) $dom->ch3,
+                            'country'    => (string) $dom->ch2,
                         ];
 
                         DB::table('epgdata_channel')->insert($insert_data);
-
                     }
 
-                    if(isset($dom->g0)) {//Genre file
+                    if (isset($dom->g0)) {//Genre file
 
                         $insert_data = [
-                            'id' => null,
+                            'id'      => null,
                             'genreid' => (int) $dom->g0,
-                            'name' => (string) $dom->g1
+                            'name'    => (string) $dom->g1,
                         ];
 
                         DB::table('epgdata_genre')->insert($insert_data);
-
                     }
-
                 }
-
             }
 
             File::delete($file->getPathname());
-
         }
-
     }
-
 }
--- app/Console/Commands/netflixImport.php
+++ app/Console/Commands/netflixImport.php
@@ -2,13 +2,13 @@
 
 namespace App\Console\Commands;
 
+use GuzzleHttp\Client;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use Imdb\Config;
 use Imdb\Title;
 use Statickidz\GoogleTranslate;
-use GuzzleHttp\Client;
 
 class netflixImport extends Command
 {
@@ -43,7 +43,6 @@ class netflixImport extends Command
      */
     public function handle()
     {
-
         $config = new Config();
         $config->language = 'de-DE,de,en';
         $google_translator = new GoogleTranslate();
@@ -53,70 +52,62 @@ class netflixImport extends Command
          */
         DB::table('netflix_movie')->truncate();
 
-
         /*
          * New on netflix
          */
         $client = new Client();
-        $result = $client->request('GET', env('NETFLIX_API_URL') . '?q=get%3Anew7%3ADE&p=1&t=ns&st=adv', [
+        $result = $client->request('GET', env('NETFLIX_API_URL').'?q=get%3Anew7%3ADE&p=1&t=ns&st=adv', [
             'headers' => [
-                env('NETFLIX_API_HEADER_HOST_KEY') => env('NETFLIX_API_HEADER_HOST_VALUE'),
-                env('NETFLIX_API_HEADER_SECRET_KEY') => env('NETFLIX_API_HEADER_SECRET_VALUE')
-            ]
+                env('NETFLIX_API_HEADER_HOST_KEY')   => env('NETFLIX_API_HEADER_HOST_VALUE'),
+                env('NETFLIX_API_HEADER_SECRET_KEY') => env('NETFLIX_API_HEADER_SECRET_VALUE'),
+            ],
         ]);
 
         $response = $result->getBody();
 
-        if(isset($response)) {
+        if (isset($response)) {
             $netflix_new_movies = json_decode($response, true);
             unset($response);
 
-            foreach ($netflix_new_movies['ITEMS'] AS $netflix_new_movies) {
-
-                if(empty($netflix_new_movies['imdbid'])) {
+            foreach ($netflix_new_movies['ITEMS'] as $netflix_new_movies) {
+                if (empty($netflix_new_movies['imdbid'])) {
                     continue;
                 }
 
                 $imdb = new Title($netflix_new_movies['imdbid'], $config);
                 $genre = implode(',', $imdb->genres());
 
-                if(isset($genre) && Str::contains($genre, 'Horror')) {
-
+                if (isset($genre) && Str::contains($genre, 'Horror')) {
                     $netflix_movie_translation = DB::table('netflix_movie_translation')->where('imdbid', $netflix_new_movies['imdbid'])->first();
 
-                    if(!isset($netflix_movie_translation->imdbid)) {
-
+                    if (!isset($netflix_movie_translation->imdbid)) {
                         $translated_synopsis = $google_translator->translate('en', 'de', $netflix_new_movies['synopsis']);
                         sleep(2);
 
                         $insert_data = [
-                            'id' => null,
-                            'imdbid' => $netflix_new_movies['imdbid'],
-                            'description' => $translated_synopsis,
+                            'id'             => null,
+                            'imdbid'         => $netflix_new_movies['imdbid'],
+                            'description'    => $translated_synopsis,
                             'description_en' => $netflix_new_movies['synopsis'],
                         ];
 
                         DB::table('netflix_movie_translation')->insert($insert_data);
-
                     }
 
                     $insert_data = [
-                        'id' => null,
-                        'netflixid' => $netflix_new_movies['netflixid'],
-                        'title' => $netflix_new_movies['title'],
-                        'image' => $netflix_new_movies['image'],
-                        'released' => $netflix_new_movies['released'],
-                        'runtime' => $netflix_new_movies['runtime'],
+                        'id'           => null,
+                        'netflixid'    => $netflix_new_movies['netflixid'],
+                        'title'        => $netflix_new_movies['title'],
+                        'image'        => $netflix_new_movies['image'],
+                        'released'     => $netflix_new_movies['released'],
+                        'runtime'      => $netflix_new_movies['runtime'],
                         'release_date' => $netflix_new_movies['unogsdate'],
-                        'imdbid' => $netflix_new_movies['imdbid'],
+                        'imdbid'       => $netflix_new_movies['imdbid'],
                     ];
 
                     DB::table('netflix_movie')->insert($insert_data);
-
                 }
-
             }
-
         }
 
         /*
@@ -124,147 +115,128 @@ class netflixImport extends Command
          */
         $genre_ids = explode(',', '10695,10944,1694,42023,45028,48303,61546,75405,75804,75930,8195,83059,8711,89585');
 
-        foreach ($genre_ids AS $genre_id) {
-
+        foreach ($genre_ids as $genre_id) {
             $client = new Client();
-            $result = $client->request('GET', env('NETFLIX_API_URL') . '?q=%7Bquery%7D-!1900%2C2019-!0%2C5-!0%2C10-!' . $genre_id . '-!Any-!Any-!Any-!gt100-!%7Bdownloadable%7D&t=ns&cl=39&st=adv&ob=Relevance&p=1&sa=and', [
+            $result = $client->request('GET', env('NETFLIX_API_URL').'?q=%7Bquery%7D-!1900%2C2019-!0%2C5-!0%2C10-!'.$genre_id.'-!Any-!Any-!Any-!gt100-!%7Bdownloadable%7D&t=ns&cl=39&st=adv&ob=Relevance&p=1&sa=and', [
                 'headers' => [
-                    env('NETFLIX_API_HEADER_HOST_KEY') => env('NETFLIX_API_HEADER_HOST_VALUE'),
-                    env('NETFLIX_API_HEADER_SECRET_KEY') => env('NETFLIX_API_HEADER_SECRET_VALUE')
-                ]
+                    env('NETFLIX_API_HEADER_HOST_KEY')   => env('NETFLIX_API_HEADER_HOST_VALUE'),
+                    env('NETFLIX_API_HEADER_SECRET_KEY') => env('NETFLIX_API_HEADER_SECRET_VALUE'),
+                ],
             ]);
 
             $response = $result->getBody();
 
-            if(isset($response)) {
-
+            if (isset($response)) {
                 $netflix_movie = json_decode($response, true);
                 unset($response);
 
-                foreach ($netflix_movie['ITEMS'] AS $netflix_movie) {
-
-                    if(empty($netflix_movie['imdbid'])) {
+                foreach ($netflix_movie['ITEMS'] as $netflix_movie) {
+                    if (empty($netflix_movie['imdbid'])) {
                         continue;
                     }
 
                     $imdb = new Title($netflix_movie['imdbid'], $config);
                     $genre = implode(',', $imdb->genres());
 
-                    if(isset($genre) && Str::contains($genre, 'Horror')) {
-
+                    if (isset($genre) && Str::contains($genre, 'Horror')) {
                         $netflix_movie_translation = DB::table('netflix_movie_translation')->where('imdbid', $netflix_movie['imdbid'])->first();
 
-                        if(!isset($netflix_movie_translation->imdbid)) {
-
+                        if (!isset($netflix_movie_translation->imdbid)) {
                             $translated_synopsis = $google_translator->translate('en', 'de', $netflix_movie['synopsis']);
                             sleep(2);
 
                             $insert_data = [
-                                'id' => null,
-                                'imdbid' => $netflix_movie['imdbid'],
-                                'description' => $translated_synopsis,
-                                'description_en' => $netflix_movie['synopsis']
+                                'id'             => null,
+                                'imdbid'         => $netflix_movie['imdbid'],
+                                'description'    => $translated_synopsis,
+                                'description_en' => $netflix_movie['synopsis'],
                             ];
 
                             DB::table('netflix_movie_translation')->insert($insert_data);
-
                         }
 
                         $translated_synopsis = $google_translator->translate('en', 'de', $netflix_movie['synopsis']);
                         sleep(2);
 
-                        if(!empty($translated_synopsis)) {
+                        if (!empty($translated_synopsis)) {
                             $netflix_movie['synopsis'] = $translated_synopsis; // just overwrite
                         }
 
                         $insert_data = [
-                            'id' => null,
+                            'id'        => null,
                             'netflixid' => $netflix_movie['netflixid'],
-                            'title' => $netflix_movie['title'],
-                            'image' => $netflix_movie['image'],
-                            'released' => $netflix_movie['released'],
-                            'runtime' => $netflix_movie['runtime'],
-                            'date' => $netflix_movie['unogsdate'],
-                            'imdbid' => $netflix_movie['imdbid'],
-                            'type' => $imdb->movietype(),
+                            'title'     => $netflix_movie['title'],
+                            'image'     => $netflix_movie['image'],
+                            'released'  => $netflix_movie['released'],
+                            'runtime'   => $netflix_movie['runtime'],
+                            'date'      => $netflix_movie['unogsdate'],
+                            'imdbid'    => $netflix_movie['imdbid'],
+                            'type'      => $imdb->movietype(),
                         ];
 
                         DB::table('netflix_movie')->insert($insert_data);
-
                     }
-
                 }
-
             }
-
         }
 
         /*
          * Expiring netflix movies
          */
         $client = new Client();
-        $result = $client->request('GET', env('NETFLIX_API_URL') . '?q=get%3Aexp%3ADE&t=ns&st=adv&p=1', [
+        $result = $client->request('GET', env('NETFLIX_API_URL').'?q=get%3Aexp%3ADE&t=ns&st=adv&p=1', [
             'headers' => [
-                env('NETFLIX_API_HEADER_HOST_KEY') => env('NETFLIX_API_HEADER_HOST_VALUE'),
-                env('NETFLIX_API_HEADER_SECRET_KEY') => env('NETFLIX_API_HEADER_SECRET_VALUE')
-            ]
+                env('NETFLIX_API_HEADER_HOST_KEY')   => env('NETFLIX_API_HEADER_HOST_VALUE'),
+                env('NETFLIX_API_HEADER_SECRET_KEY') => env('NETFLIX_API_HEADER_SECRET_VALUE'),
+            ],
         ]);
 
         $response = $result->getBody();
 
-        if(isset($response)) {
+        if (isset($response)) {
             $netflix_expired_movies = json_decode($response, true);
             unset($response);
 
-            foreach ($netflix_expired_movies['ITEMS'] AS $netflix_expired_movie) {
-
-                if(empty($netflix_expired_movie['imdbid'])) {
+            foreach ($netflix_expired_movies['ITEMS'] as $netflix_expired_movie) {
+                if (empty($netflix_expired_movie['imdbid'])) {
                     continue;
                 }
 
                 $imdb = new Title($netflix_expired_movie['imdbid'], $config);
                 $genre = implode(',', $imdb->genres());
 
-                if(isset($genre) && Str::contains($genre, 'Horror')) {
-
+                if (isset($genre) && Str::contains($genre, 'Horror')) {
                     $netflix_movie_translation = DB::table('netflix_movie_translation')->where('imdbid', $netflix_expired_movie['imdbid'])->first();
 
-                    if(!isset($netflix_movie_translation->imdbid)) {
-
+                    if (!isset($netflix_movie_translation->imdbid)) {
                         $translated_synopsis = $google_translator->translate('en', 'de', $netflix_expired_movie['synopsis']);
                         sleep(2);
 
                         $insert_data = [
-                            'id' => null,
-                            'imdbid' => $netflix_expired_movie['imdbid'],
-                            'description' => $translated_synopsis,
-                            'description_en' => $netflix_expired_movie['synopsis']
+                            'id'             => null,
+                            'imdbid'         => $netflix_expired_movie['imdbid'],
+                            'description'    => $translated_synopsis,
+                            'description_en' => $netflix_expired_movie['synopsis'],
                         ];
 
                         DB::table('netflix_movie_translation')->insert($insert_data);
-
                     }
 
                     $insert_data = [
-                        'id' => null,
-                        'netflixid' => $netflix_expired_movie['netflixid'],
-                        'title' => $netflix_expired_movie['title'],
-                        'image' => $netflix_expired_movie['image'],
-                        'released' => $netflix_expired_movie['released'],
-                        'runtime' => $netflix_expired_movie['runtime'],
+                        'id'          => null,
+                        'netflixid'   => $netflix_expired_movie['netflixid'],
+                        'title'       => $netflix_expired_movie['title'],
+                        'image'       => $netflix_expired_movie['image'],
+                        'released'    => $netflix_expired_movie['released'],
+                        'runtime'     => $netflix_expired_movie['runtime'],
                         'expire_date' => $netflix_expired_movie['unogsdate'],
-                        'imdbid' => $netflix_expired_movie['imdbid'],
-                        'type' => $imdb->movietype(),
+                        'imdbid'      => $netflix_expired_movie['imdbid'],
+                        'type'        => $imdb->movietype(),
                     ];
 
                     DB::table('netflix_movie')->insert($insert_data);
-
                 }
-
             }
-
         }
-
     }
-
 }
