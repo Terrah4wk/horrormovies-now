23e9e3cd91bf39adb7ef45199ce0fdd8b8c78f81
Julian Vieser <julian.vieser@gmail.com>
2020-06-12 22:08:05 +0200

    added imprint template

---
+++ resources/views/page_imprint.blade.php
@@ -0,0 +1,14 @@
+ 
+@extends('layout')
+
+@section('content')
+
+<section class="jumbotron text-center">
+    <div class="container">
+
+    {!! $imprint_content !!}
+
+</div>
+</section>
+
+@endsection
